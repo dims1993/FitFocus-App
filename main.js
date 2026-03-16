5489ae631948275603bfc0ab504a2fa73ad490ad
David <dimsepp@gmail.com>
2026-03-16 23:57:51 +0100

    feat: implement fetch to get data from external API

--- main.js
+++ main.js
@@ -14,3 +14,33 @@ contenedor.innerHTML = misEjercicios
     (ex) => `<div class="card"><h3>${ex.nombre}</h3><p>${ex.musculo}</p></div>`,
   )
   .join("");
+
+// Llamada a nuestra API
+
+async function cargarEjercicios() {
+  console.log("Fetching data..."); // Aviso
+
+  // Va a por los datos
+  const respuesta = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=5",
+  );
+
+  // Abrimos el paquete (convertimos a JSON)
+  const datos = await respuesta.json();
+
+  // Pintar en pantalla
+  const contenedor = document.getElementById("exercise-container");
+
+  contenedor.innerHTML = datos
+    .map((item) => {
+      return `
+      <div class="card">
+      <h3>$item.title.toUpperCase{}</h3>
+      <p>Id del ejercicio: ${item.id}</p>
+      </div>
+      `;
+    })
+    .join("");
+}
+
+cargarEjercicios();
